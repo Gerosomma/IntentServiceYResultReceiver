18f2e1bcfa34841c7ae08536fb83afc5ad7e9e09
Geronimo.Somma <sommagero@gsoft.com.uy>
2019-12-11 20:05:22 -0300

    esqueleto para ResultReceiver

--- app/src/main/java/com/somma/intentserviceyresultreceiver/MainActivity.java
+++ app/src/main/java/com/somma/intentserviceyresultreceiver/MainActivity.java
@@ -3,12 +3,29 @@ package com.somma.intentserviceyresultreceiver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MENSAJE = "EXTRA_MENSAJE";
+    protected TextView tvMensaje;
+    private String mensajePredeterminado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMensaje = (TextView)findViewById(R.id.tvMensaje);
+        mensajePredeterminado = getResources().getString(R.string.tvMensaje);
+    }
+
+    public void btnRestaurarOnClick(View view) {
+
+    }
+
+    public void btnProcesarOnClick(View view) {
+
     }
 }
